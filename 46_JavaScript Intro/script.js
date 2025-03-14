2b1146d41a985a9a620206e099d8d058184b4255
nilesh-0001 <castreader05@gmail.com>
2025-03-14 23:09:15 +0530

    Js Intro

---
+++ 46_JavaScript Intro/script.js
@@ -0,0 +1,13 @@
+// alert("Hello World");
+console.log("Code is Running....")
+console.log("boy")
+console.log("Girl")
+let a =prompt("Enter Your Number")
+console.log("Your Number is " + a)
+let isTrue = confirm("What is Your answer Yes or No")
+if(isTrue){
+    console.log("The answer is Yes")
+}
+else{
+    console.log("The answer is No")
+}
