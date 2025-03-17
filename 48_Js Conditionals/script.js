51e83c8f1902d6cdbaae0fddc0cf90be818b154a
nilesh-0001 <castreader05@gmail.com>
2025-03-17 23:13:44 +0530

    Conditionals and Operators

---
+++ 48_Js Conditionals/script.js
@@ -0,0 +1,31 @@
+console.log("Conditional Tutorials");
+
+let age = 0;
+
+let relax = 2;
+
+// Operators
+// age %= relax
+// age += relax
+// console.log(age)
+// console.log(age + relax)
+// console.log(age - relax)
+// console.log(age * relax)
+// console.log(age / relax)
+// console.log(age ** relax)
+// console.log(age % relax)
+// Conditional
+
+if (age != 0) {
+  console.log("You can Drive");
+} else if (age == 0) {
+  console.log("you cannot drive Baby");
+} else {
+  console.log("You cannnot Drive");
+}
+
+let a = 6;
+let b = 5;
+
+let c = a > b ? a - b : a + b;
+console.log(c);
