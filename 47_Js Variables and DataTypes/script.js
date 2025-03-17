4b53217db156276c8f1d4aa947d4562fa1bfd613
nilesh-0001 <castreader05@gmail.com>
2025-03-17 20:09:45 +0530

    Variables and DataTypes

---
+++ 47_Js Variables and DataTypes/script.js
@@ -0,0 +1,51 @@
+// console.log("In this session we are gonna Talk about variables and DataTypes");
+
+// let a = 6;
+// let b = 3;
+// let c = "Nishu";
+// console.log(a);
+
+// console.log(a + b + 11);
+
+// console.log(typeof a, typeof b, typeof c);
+
+// {
+//   let a = 7;
+//   console.log(a);
+// }
+// console.log(a);
+// const a1 = 6;
+// a1 = a1 + 1;
+
+// console.log("There are 2 Types of DataTypes");
+
+// // Primitive Datatypes
+
+// 
+/* Null
+  Number
+  BigInt
+  Boolean
+  undefined
+  Symbol
+  String
+*/
+let x = "Nilesh";
+let t = 23;
+let q = BigInt("45") + BigInt("45");
+let w = null;
+let r = undefined;
+const k = true;
+
+console.log(typeof x,typeof t,typeof q,typeof w,typeof r,typeof k)
+
+
+const me = {
+    Name : "Nishu",
+    Age : 34,
+    Qualification : "12th Pass",
+    Job : "Full Stack",
+}
+
+me.Salary = 1500000
+console.log(me)
