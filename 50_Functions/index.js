ada6cbb18253fbb8a098d070b796ac5ba8ea7cac
nilesh-0001 <castreader05@gmail.com>
2025-03-18 23:37:03 +0530

    Function

---
+++ 50_Functions/index.js
@@ -0,0 +1,22 @@
+function Addition(a , b , c) {
+    return console.log(a+b+c)
+    
+}
+
+Addition(1,2,3)
+function nice (name){
+    console.log("My name is " + name )
+    console.log( name + " is good")
+    console.log( name + " is Bad")
+    console.log( name + " is smart")
+}
+nice("Nilesh")
+nice("Nishu")
+
+
+const sum = (x)=>{
+    console.log("I am a arrow Function",x)
+}
+sum(2)
+sum(7)
+sum(9)
