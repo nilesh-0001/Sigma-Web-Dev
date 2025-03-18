5555f3f115bfeb2638c5efb0666bf5733b989fe5
nilesh-0001 <castreader05@gmail.com>
2025-03-18 19:39:10 +0530

    Loops

---
+++ 49_Js on Loops/script.js
@@ -0,0 +1,32 @@
+// console.log("Loops");
+
+// let a = 2;
+
+// for (i = 0; i < 100; i++) {
+//   console.log(a + i);
+// }
+// let obj = {
+//     Name : "Nilesh",
+//     Role : "Frontend",
+//     Company : "Dreamer"
+// }
+// for(const key in obj){
+//     const element =obj[key]
+//     console.log(key, element)
+//     console.log(element)
+// }
+
+// for(const c of "Nishu"){
+//     console.log(c)
+// }
+
+// let i = 0
+// while (i<6) {
+//     console.log(i)
+//     i++;
+// }
+let i = 8;
+do {
+  console.log(i);
+  i++;
+} while (i < 6);
