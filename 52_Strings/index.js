e87bb6ab2d264f0357eeaa6a2124a4bc6fc6cac7
nilesh-0001 <castreader05@gmail.com>
2025-03-28 12:07:14 +0530

    Strings

---
+++ 52_Strings/index.js
@@ -0,0 +1,28 @@
+console.log("This session is about Strings");
+
+// String are used to store and Manipulate Text
+
+let a = "Nishu";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+
+// Template literal 
+let x = "Anshul"
+let y = "Sera"
+console.log(`${x} and ${y} are best Friends`)
+
+
+console.log(a.length)
+console.log(a.toUpperCase())
+console.log(a.toLowerCase())
+console.log(x.slice(1,4))
+console.log(x.slice(0,5))
+console.log(x.replace("hu","kal"))
+console.log(x.concat(y," Nilesh"))
+// String is imutable it is not gonna change the orignal variable string value
+console.log(x.charAt(0))
+console.log(x.indexOf("hul"))
+console.log(x.startsWith(An))
