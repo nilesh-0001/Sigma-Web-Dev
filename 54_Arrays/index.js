050c9b026e8dea8569b71a07a7565a77718557f0
nilesh-0001 <castreader05@gmail.com>
2025-03-30 23:33:13 +0530

    Array or array methods

--- 54_Arrays/index.js
+++ 54_Arrays/index.js
@@ -13,28 +13,27 @@ console.log(typeof arr);
 arr[0] = true;
 console.log(arr);
 
-// tostringmethod
-let a = [1, 2, 3, 4, 5, 6];
+// // tostringmethod
 console.log(a.toString());
 
-// join method
+// // join method
 console.log(a.join(" --- "));
 
-// pop method
+// // pop method
 // remove the last element
 console.log(a.pop());
 
-// push method
+// // push method
 console.log(a.push(12));
 console.log(a);
 
-// shift method
+// // shift method
 // removes the first element
 console.log(a.shift());
 console.log(a);
 
-// unshift method
-// add element in the first
+// // unshift method
+// // add element in the first
 console.log(a.unshift(45));
 console.log(a);
 
@@ -42,7 +41,7 @@ console.log(a);
 console.log(delete a[0]);
 console.log(a);
 
-// concat method use for adding two or more than two arrays
+// // concat method use for adding two or more than two arrays
 let a1 = [1, 2, 3];
 let a2 = [11, 12, 13];
 let a3 = [21, 22, 33];
@@ -51,7 +50,7 @@ console.log(a1.concat(a2, a3));
 
 console.log(a1, a2, a3);
 
-// splice method
+// // splice method
 let x = [1, 2, 3, 4, 5, 6, 7];
 console.log(x.splice(1,2))
 console.log(x)
@@ -60,10 +59,84 @@ let y = [11, 12, 13, 14, 15, 16]
 y.splice(3,3,40,50,60)
 console.log(y)
 
-// slice method
+// // slice method
 let z = [1, 2, 3, 4, 5]
 console.log(z.slice(2))
 console.log(z.slice(1,3))
 
 // reverse
 console.log(z.reverse())
+
+
+
+// Looping through arrays
+
+// For Loop with Array
+let num = [11, 21, 31, 41, 51];
+for (i = 0; i < num.length; i++) {
+  console.log(num[i]);
+}
+
+// foreach method 
+num.forEach((element)=>{
+   console.log( element+1)
+})
+
+num.forEach((value,element,arr)=>{
+    console.log(value,element,arr)
+})
+
+let obj = {
+    a:1,
+    b:2,
+    c:3
+}
+
+for (const key in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        console.log(key,element)
+    }
+}
+
+
+let a = [1,2,3,4]
+for (const value of num) {
+    console.log(value)
+}
+
+
+let arr = [1,11,45,14,5,8]
+// let narr = []
+
+for (let index = 0; index < arr.length; index++) {
+    const element = arr[index];
+    narr.push(element**2)
+}
+
+// Alternative method 
+let narr = arr.map((e,inex,arr)=>{
+    return e**2
+})
+console.log(narr)
+
+const greatethanseven = (e)=>{
+    if(e>7){
+        return true
+    }
+    return false
+}
+
+
+console.log(narr.filter(greatethanseven))
+
+
+let arr2 = [1,2,3,4,5]
+
+const red = (a,b)=>{
+    return a*b
+}
+
+console.log(arr2.reduce(red))
+let n = "Nilesh"
+console.log(Array.from(n))
