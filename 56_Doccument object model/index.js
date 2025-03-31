af663b7e460f48d7069651d0918d5a181b7d4b15
nilesh-0001 <castreader05@gmail.com>
2025-03-31 14:02:05 +0530

    Document object model

---
+++ 56_Doccument object model/index.js
@@ -0,0 +1,8 @@
+document.title = "Harry is Good"
+console.log(document.title)
+console.log(document.body)
+
+document.body.style.background = "blue"
+
+// Basically the Dom  and bom is used to select particular item on the browser and also for creating any cahnges in them.
+// 0$ is the selected item on the html document
